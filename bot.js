d8dc35385f9ba3fe5076db0e7eff008d8c30a36c
ShadowX <32039975+ShadwX57@users.noreply.github.com>
2018-09-16 13:52:51 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,299 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const prefix = "b!"
+
+
+console.log('Hello')
+
+client.on('message', message => {
+              if(!message.channel.guild) return;
+    if(message.content.startsWith('b!bc')) {
+    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+    let copy = "MaxBot";
+    let request = `Requested By ${message.author.username}`;
+    if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
+    msg.react('✅')
+    .then(() => msg.react('❌'))
+    .then(() =>msg.react('✅'))
+
+
+
+
+
+
+
+    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+       let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+    reaction1.on("collect", r => {
+    message.channel.send(`☑ |   ${message.guild.members.size} يتم ارسال البرودكاست الى عضو `).then(m => m.delete(5000));
+    message.guild.members.forEach(m => {
+    var bc = new
+       Discord.RichEmbed()
+       .setColor('RANDOM')
+       .setTitle('البرودكاست') .addField('السيرفر', message.guild.name) .addField('المرسل', message.author.username)
+       .addField('الرساله', args)
+       .setThumbnail(message.author.avatarURL)
+       .setFooter(copy, client.user.avatarURL);
+    m.send({ embed: bc })
+    msg.delete();
+    })
+    })
+    reaction2.on("collect", r => {
+    message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
+    msg.delete();
+    })
+    })
+    }
+    })
+    
+const { Client } = require('discord.js');
+client.on(`ready`, () => console.log(`Ready!`))
+const channels = {};
+
+
+
+client.on('voiceStateUpdate',async function(oldmember, member) {
+if(member.user.bot) return;
+if(member.voiceChannel === undefined && channels[member.id]) {
+console.log(member.guild.members.filter(m => m.voiceChannelID === channels[member.id].channel).size)
+if(member.guild.members.filter(m => m.voiceChannelID === channels[member.id].channel).size < 1) {
+member.guild.channels.get(channels[member.id].channel).delete();
+channels[member.id].channel = undefined;
+}
+}
+if(oldmember.voiceChannel !== undefined || member.voiceChannel !== undefined) {
+if(member.voiceChannelID === '473135327230558209') {
+member.guild.createChannel(member.displayName, "voice", [{
+id: member.id,
+allow: ['CONNECT'],
+}, {
+id: member.guild.id,
+deny: ['CONNECT']
+}]).then((channel)=> {
+    const parent = member.guild.channels.get('473135327230558209').parentID
+    channel.setParent(parent);
+    if(!channels[member.id]) channels[member.id] = {
+        channel: channel.id,
+        }
+member.user.send(`Hey **${member.displayName}** I've created a channel for you!
+------------------------------------------------------------
+Use \`\`b!unlock [@user | all]\`\` to unlock for a specify or for all.
+Use \`\`b!lock [@user | all]\`\` to lock & kick for a specify or for all in your voice channel.
+Use \`\`b!rename [new name]\`\` to rename your voice channel name.
+------------------------------------------------------------
+`)
+member.setVoiceChannel(channel.id);
+})
+} else return undefined;
+}
+});
+
+client.on(`message`, async message => {
+let args = message.content.trim().split(" ").slice(1); //substring(prefix.length) before split(" ") if you had a prefix.
+let user = message.mentions.users.first();
+if(message.content.startsWith("b!unlock")) {
+if(channels[message.author.id] !== undefined) {
+if(user) {
+if(message.guild.channels.get(channels[message.author.id].channel).permissionsFor(user.id).has(`CONNECT`)) return message.channel.send(`**The user already can connect to your voice channel**\n to lock & kick user use \`\`!lock\`\` `);
+message.guild.channels.get(channels[message.author.id].channel).overwritePermissions(user.id, {
+CONNECT: true
+}).then(message.channel.send(`**${user.username}** can connect to your room now!`))
+}
+else if(args.includes("all")) {
+message.guild.channels.get(channels[message.author.id].channel).overwritePermissions(message.guild.id, {
+CONNECT: true
+}).then(message.channel.send("**Everyone** can connect to your room now!"));
+} else return message.channel.send(`**Usage: $unlock [all | @user]**`)
+}
+}
+if(message.content.startsWith("b!lock")) {
+if(channels[message.author.id] !== undefined) {
+if(user) {
+if(!message.guild.channels.get(channels[message.author.id].channel).permissionsFor(user.id).has(`CONNECT`)) return message.channel.send(`**The user already cannot connect to your voice channel**`);
+try {
+if(message.guild.members.get(user.id).voiceChannelID === channels[message.author.id].channel) {
+message.guild.members.get(user.id).setVoiceChannel('479803166498619402'); // المكان الي راح ينحطوله بعد ما يصير لهم lock
+}
+} catch (error) {
+console.log(error)
+}
+message.guild.channels.get(channels[message.author.id].channel).overwritePermissions(user.id, {
+CONNECT: false
+}).then(message.channel.send(`:x: **${user.username}** cannot connect to your room now!`))
+}
+else if(args.includes("all")) {
+message.guild.channels.get(channels[message.author.id].channel).overwritePermissions(message.guild.id, {
+CONNECT: false
+}).then(message.channel.send(":x: **Everyone** cannot connect to your room now!"));
+} else return message.channel.send(`**Usage: b!lock [all | @user]**`)
+}
+}
+if(message.content.startsWith("b!rename")) {
+if(channels[message.author.id] !== undefined) {
+if(args.length <= 0) return message.channel.send(`:scroll: **Hmmm the name please*`);
+if(message.content.length > 7+15) return message.channel.send(`:x: It appears that's the max letters allowed is **15**.`)
+const oldName = await message.guild.channels.get(channels[message.author.id].channel).name
+message.channel.send(`:pencil2: Renamed **\`\`${oldName}\`\`** to **\`\`${args.join(" ").toString()}\`\`** alright?`)
+message.guild.channels.get(channels[message.author.id].channel).setName(args.join(" ").toString());
+}
+ }
+});
+
+  const moment = require('moment')
+  client.on('message', async message => {
+    let date = moment().format('Do MMMM YYYY , hh:mm');
+    let User = message.mentions.users.first();
+    let Reason = message.content.split(" ").slice(3).join(" ");
+    let messageArray = message.content.split(" ");
+    let time = messageArray[2];
+    if(message.content.startsWith(prefix + "ban")) {
+       if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.channel.send("**- You don't have the needed permissions!**");
+       if(!User) message.channel.send("**- Mention someone!**");
+       if(User.id === client.user.id) return message.channel.send("**- You cannot ban me!**");
+       if(User.id === message.guild.owner.id) return message.channel.send("**- You cannot ban the owner of the server!**");
+       if(!time) return message.channel.send("**- Supply a duration!**");
+       if(!time.match(/[1-7][s,m,h,d,w]/g)) return message.channel.send('**- Supply a real time!**');
+       if(!Reason) message.channel.send("**- Supply a reason!**");
+       let banEmbed = new Discord.RichEmbed()
+       .setAuthor(`You have been banned from ${message.guild.name} !`)
+       .setThumbnail(message.guild.iconURL || message.guild.avatarURL)
+       .addField('- Banned By: ',message.author.tag,true)
+       .addField('- Reason:',`${Reason}`)
+       .addField('- Banned At:',`${message.createdAt}`)
+       .addField('- Duration:',`${time}`)
+       .setFooter(message.author.tag,message.author.avatarURL);
+     message.guild.channels.find('name',  'incidents').sendEmbed(banEmbed)  .then(() => message.guild.member(User).ban({reason: Reason}));
+     User.send(`You Are Has Been Banned In BreackMC Server Reason: ${Reason}`)
+       .then(() => message.channel.send(`**# Done! I banned: ${User}**`)).then(() => { setTimeout(() => {
+           message.guild.unban(User);
+       }, mmss(time));
+    });
+   } 
+});
+
+client.on('message', async message => {
+    if(message.content.startsWith(prefix + "voicesetup")) {
+    if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply(':x: **ليس لديك الصلاحيات الكافية**');
+    if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply(':x: **ليس معي الصلاحيات الكافية**');
+    var args = message.content.split(' ').slice(1).join(' ');
+    if(args && !args.includes(0)) return message.channel.send(':negative_squared_cross_mark: » فشل اعداد الروم الصوتي .. __يجب عليك كتابة 0 في اسم الروم__');
+    if(!args) args = `Voice Online「 ${message.guild.members.filter(s => s.voiceChannel).size} 」`;
+    message.channel.send(':white_check_mark: » تم عمل الروم الصوتي بنجاح');
+    message.guild.createChannel(`${args.replace(0, message.guild.members.filter(s => s.voiceChannel).size)}`, 'voice').then(c => {
+      c.overwritePermissions(message.guild.id, {
+        CONNECT: false,
+        SPEAK: false
+      });
+      setInterval(() => {
+        c.setName(`${args.replace(0, message.guild.members.filter(s => s.voiceChannel).size)}`).catch(err => {
+          if(err) return;
+        });
+      },3000);
+    });
+    }
+  });
+  
+  client.on('message', async message => {
+  if(message.content.startsWith(prefix + "help")) {
+let embed = new Discord.RichEmbed()
+.addField('b!mute', 'Mute someone')
+.addField('b!ban', 'Banned someone')
+.addField('b!warn', 'Warning someone')
+.addField('b!serverinfo', 'Serverinformation')
+.addField('b!boinfo', 'Botinformation')
+.addField('b!clear', 'Clearchat')
+.addField('b!bc', 'Broadcast')
+.addField('b!addrole', 'Addrole / Rank')
+.addField('b!removerole', 'Removerole / Rank')
+message.channel.sendEmbed(embed)
+}
+  
+  client.on('message', async message => {
+    if(message.content.startsWith(prefix + "userssetup")) {
+    if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply(':x: **ليس لديك الصلاحيات الكافية**');
+    if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply(':x: **ليس معي الصلاحيات الكافية**');
+    var args = message.content.split(' ').slice(1).join(' ');
+    if(args && !args.includes(0)) return message.channel.send(':negative_squared_cross_mark: » فشل اعداد الروم الصوتي .. __يجب عليك كتابة 0 في اسم الروم__');
+    if(!args) args = `Users •「 ${message.guild.memberCount} 」`;
+    message.channel.send(':white_check_mark: » تم عمل الروم الصوتي بنجاح');
+    message.guild.createChannel(`${args.replace(0, message.guild.members.filter(s => s.voiceChannel).size)}`, 'voice').then(c => {
+      c.overwritePermissions(message.guild.id, {
+        CONNECT: false,
+        SPEAK: false
+      });
+      setInterval(() => {
+c.setName(`${args.replace(0, message.guild.memberCount)}`).catch(err => {  
+    if(err) return;
+        });
+      },3000);
+    });
+    }
+  });
+  
+    client.on('message', async message => {
+  const developers = ["349013875250823170", "429972030092476437"]
+const adminprefix = "b!";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**✅**`)
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
+  
+
+
+    const  child_process  =  require("child_process");
+    client.on('ready'  ,  function  (){
+var  time  =  7200000;
+client.setInterval(function()  {
+        client.destroy();
+                child_process.fork(__dirname  +  "/bc.js");
+    },  time);
+});
+
+    client.on('ready'  ,  function  (){
+var  time  =  7200000;
+client.setInterval(function()  {
+        client.destroy();
+                child_process.fork(__dirname  +  "/index.js");
+    },  time);
+});
+
+
+
+
+
+})
+})
+
+client.login(process.env.BOT_TOKEN)
